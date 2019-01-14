bb0d068a684494cba41ab6a8f23dcdd53a30b170
Natalia <natalia1andriychuk@gmail.com>
2019-01-13 19:33:53 -0500

    adding census data and congigEnv files

---
+++ config.py
@@ -0,0 +1,5 @@
+# Enter your API keys
+
+census_api_key = "f66a286b88c296c7f7ddf0375c8143d5fb416b40"
+
+gkey = "AIzaSyCq8cIUVxe9rQIpIHdcjiRu3yiwsarIlSk"
---
+++ configEnv.py
@@ -0,0 +1,5 @@
+# Enter your API keys
+
+census_api_key = "PUT YOUR CENSUS KEY HERE!"
+
+gkey = "PUT YOUR GMAPS KEY HERE!"
