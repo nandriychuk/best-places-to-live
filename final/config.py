0ed3270f94fdff84174da3603f755a7d6cc0f785
Laura Lancheros <lauralancheros@yahoo.com>
2019-01-19 09:06:41 -0500

    Final materials for Census Commuting
    Includes jupyter notebook, excel model and images.

---
+++ final/config.py
@@ -0,0 +1,6 @@
+# Enter your API keys
+
+census_api_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+gkey = "AIzaSyBgwUpfqRABk2RQmdHFgB-fBYnohMnwBNI"
+b_api_key = "4a7c29c7021b22dab4ce0d7a25634534"
+
